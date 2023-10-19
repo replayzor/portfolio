76392fdcbf2d4c2da1131270c98d15cbec4bc375
replayzor <oltean.ionut1989@gmail.com>
2023-10-19 10:06:37 +0300

    feat. Add routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
+// react router dom imports
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import AppLayout from "./layouts/AppLayout";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <AppLayout />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+			{
+				path: "skills",
+				element: <Skills />,
+			},
+			{
+				path: "projects",
+				element: <Projects />,
+			},
+			{
+				path: "contact",
+				element: <Contact />,
+			},
+		],
+	},
+]);
+
 function App() {
-	return (
-		<h1 className="text-orange-600 text-3xl font-bold underline">
-			Hello world!
-		</h1>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
