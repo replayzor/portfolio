38b2d6997572a27422d4cabecd8907cc8858aa8a
replayzor <oltean.ionut1989@gmail.com>
2023-10-19 10:06:12 +0300

    feat. Add data for projects, skills and links

--- src/data.js
+++ src/data.js
@@ -1,30 +1,32 @@
+// library imports
 import { nanoid } from "nanoid";
 import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
 
 export const links = [
-	{ id: nanoid(), href: "#home", text: "home" },
-	{ id: nanoid(), href: "#about", text: "about" },
-	{ id: nanoid(), href: "#skills", text: "skills" },
-	{ id: nanoid(), href: "#projects", text: "projects" },
+	{ id: nanoid(), href: "home", text: "home" },
+	{ id: nanoid(), href: "about", text: "about" },
+	{ id: nanoid(), href: "skills", text: "skills" },
+	{ id: nanoid(), href: "projects", text: "projects" },
+	{ id: nanoid(), href: "contact", text: "contact" },
 ];
 
 export const skills = [
 	{
 		id: nanoid(),
 		title: "HTML&CSS",
-		icon: <FaHtml5 className="w-16 h-16 text-emerald-500" />,
+		// icon: <FaHtml5 className="w-16 h-16 text-emerald-500" />,
 		text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
 	},
 	{
 		id: nanoid(),
 		title: "Javascript",
-		icon: <FaJs className="w-16 h-16 text-emerald-500" />,
+		// icon: <FaJs className="w-16 h-16 text-emerald-500" />,
 		text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
 	},
 	{
 		id: nanoid(),
 		title: "React",
-		icon: <FaReact className="w-16 h-16 text-emerald-500" />,
+		// icon: <FaReact className="w-16 h-16 text-emerald-500" />,
 		text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
 	},
 ];
