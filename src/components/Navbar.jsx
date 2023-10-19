837598054723998ed25c665609c74b5fe4216574
replayzor <oltean.ionut1989@gmail.com>
2023-10-19 13:15:03 +0300

    refactor: Simplify Navbar component structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,12 @@ function Navbar() {
 	return (
 		<nav className="bg-blue-200 ">
 			<div className="flex flex-col justify-between px-8 py-4 mx-auto sm:gap-x-16 sm:items-center sm:py-8 sm:flex-row max-w-7xl">
-				<h2 className="text-3xl font-bold text-orange-500">
-					Replay <span className="text-blue-600">Dev</span>
-				</h2>
-				<div className="flex gap-x-3">
+				<Link to="/">
+					<h2 className="text-3xl font-bold text-orange-500">
+						Replay <span className="text-blue-600">Dev</span>
+					</h2>
+				</Link>
+				<div className="flex pt-4 font-semibold text-orange-500 gap-x-3">
 					{links.map((link) => {
 						const { id, href, text } = link;
 						return (
